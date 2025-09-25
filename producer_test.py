553e2a5c74d3261a01dd475ff6949b529c904fb9
Derek Fintel <s542635@nwmissouri.edu>
2025-09-24 19:25:14 -0500

    testing script folder & cleanup

--- producer_test.py
+++
@@ -1,4 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers="localhost:9092")
-producer.send("buzzline", b'{"test":"hello"}')
-producer.flush()
--- testtopics.py
+++
@@ -1,4 +0,0 @@
-from kafka.admin import KafkaAdminClient
-
-admin = KafkaAdminClient(bootstrap_servers="localhost:9092")
-print(admin.list_topics())
